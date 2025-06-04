1616a452aef036ad0637690ad88c1d66ce68b05a
Irene Kober <irene.kober@gmx.net>
2025-06-04 14:38:51 +0200

    documented Coin class

--- js/models/coin.class.js
+++ js/models/coin.class.js
@@ -1,12 +1,48 @@
+/**
+ * creates new Coin
+ * @class
+ */
 class Coin extends DrawableObject {
-        x = 250 + Math.random() * 1200;
-        y = 150 + Math.random() * 160; // Keeps them between 200 and 360
-        height = 100;
-        width = 100;
-        offset = { top: 35, bottom: 35, left: 35, right: 35};
+    /**
+     * horizontal position of coin, randomized within range.
+     * @type {number}
+     */
+    x = 250 + Math.random() * 1200;
 
-    constructor(){
+    /**
+     * vertical position of coin, randomized within range.
+     * Keeps coin between 200 and 360 on the screen.
+     * @type {number}
+     */
+    y = 150 + Math.random() * 160;
+
+    /**
+     *  height of  coin sprite.
+     * @type {number}
+     */
+    height = 100;
+
+    /**
+     *  width of coin sprite.
+     * @type {number}
+     */
+    width = 100;
+
+    /**
+     * Offset values used for collision detection.
+     * @type {Object}
+     * @property {number} top - Top offset.
+     * @property {number} bottom - Bottom offset.
+     * @property {number} left - Left offset.
+     * @property {number} right - Right offset.
+     */
+    offset = { top: 35, bottom: 35, left: 35, right: 35 };
+
+    /**
+     * Creates Coin instance and loads image.
+     */
+    constructor() {
         super();
-        this.loadImage('assets/img/8_coin/coin_1.png'); //super übergeordnete FUnkiton -> MovalbeObject
+        this.loadImage('assets/img/8_coin/coin_1.png'); 
     }
 }
