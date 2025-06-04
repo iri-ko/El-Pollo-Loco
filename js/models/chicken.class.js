e8acf23cbcaf5a50514981d3623679a7f813297a
Irene Kober <irene.kober@gmx.net>
2025-06-04 14:36:43 +0200

    documented Cloud class

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -1,35 +1,85 @@
+/**
+ * creates a new Chicken
+ * @class
+ */
 class Chicken extends MovableObject {
     
-    //#region attributes
-    x =  550 + Math.random() * 700;
+    //#region Attributes
+
+    /**
+     * horizontal position of Chicken, randomized within a range.
+     * @type {number}
+     */
+    x = 550 + Math.random() * 700;
+
+    /**
+     * The vertical position of the Chicken.
+     * @type {number}
+     */
     y = 360;
+
+    /**
+     * The height of the Chicken sprite.
+     * @type {number}
+     */
     height = 80;
+
+    /**
+     * The width of the Chicken sprite.
+     * @type {number}
+     */
     width = 70;
+
+    /**
+     * Movement speed of the Chicken, randomized within a range.
+     * @type {number}
+     */
     speed = 0.35 + Math.random() * 0.25;
+
+    /**
+     * Hitbox values (offset)
+     * @type {Object}
+     * @property {number} top - Top offset.
+     * @property {number} bottom - Bottom offset.
+     * @property {number} left - Left offset.
+     * @property {number} right - Right offset.
+     */
     offset = { top: 4, bottom: 4, left: 4, right: 4 };
+
     //#endregion
 
+    /**
+     * Creates Chicken instance and initializes animations.
+     */
     constructor() {
         super();
         this.loadImages(ImageHub.chicken.walk);
         this.animate();
     }
 
+    /**
+     * Starts animation loops for Chicken's movement and sprite changes.
+     */
     animate() {
-        IntervalHub.startInterval(this.animateChicken, 80, "chickenAnimate")
+        IntervalHub.startInterval(this.animateChicken, 80, "chickenAnimate");
         IntervalHub.startInterval(this.moveLeft, 17, "chickenMoveLeft");
     }
 
+    /**
+     * Plays walking animation for Chicken.
+     */
     animateChicken = () => {
         this.playAnimation(ImageHub.chicken.walk);
-    }
+    };
 
+    /**
+     * Handles death behavior of Chicken.
+     * Changes sprite moves the object off-view
+     */
     die() {
-        this.loadImage(
-            "assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png"
-        ); // Show "dead" sprite
+        this.loadImage("assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png"); 
         setTimeout(() => {
-            this.x = -9999; // Move it far off-screen instead of removing it
+            this.x = -9999;
         }, 500);
     }
 }
--- js/models/cloud.class.js
+++ js/models/cloud.class.js
@@ -1,16 +1,50 @@
+/**
+ * creates new Cloud
+ * @class
+ */
 class Cloud extends MovableObject {
+    /**
+     * height of cloud sprite.
+     * @type {number}
+     */
     height = 180;
+
+    /**
+     * movement speed of cloud.
+     * @type {number}
+     */
     speed = 0.15;
+
+    /**
+     * vertical position of cloud.
+     * @type {number}
+     */
     y = 20;
+
+    /**
+     * width of cloud sprite.
+     * @type {number}
+     */
     width = 500;
+
+    /**
+     * horizontal position of cloud, randomized within a range.
+     * @type {number}
+     */
     x = Math.random() * 500;
 
+    /**
+     * Creates Cloud instance and initializes movement.
+     */
     constructor() {
         super();
-        this.loadImage("assets/img/5_background/layers/4_clouds/w1.png"); //super übergeordnete FUnkiton -> MovalbeObject
+        this.loadImage("assets/img/5_background/layers/4_clouds/w1.png");
         this.animate();
     }
 
+    /**
+     * Starts  cloud movement animation, making it continuously move left.
+     */
     animate() {
         IntervalHub.startInterval(this.moveLeft, 17, "cloudMoveleft");
     }
